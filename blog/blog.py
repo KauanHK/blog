f304f93fe383327faaa54ca10c451e3d5c8221a9
KauanHK <kauankaestner06@gmail.com>
2024-12-12 10:38:43 -0300

    Adicionado view update()

--- blog/blog.py
+++ blog/blog.py
@@ -50,7 +50,7 @@ def create():
         if not title:
             error = 'A postagem deve ter um título.'
         
-        if not body:
+        elif not body:
             error = 'A postagem deve ter conteúdo.'
         
         if error is not None:
@@ -66,3 +66,35 @@ def create():
             return redirect(url_for('blog.index'))
         
     return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id: int):
+
+    post = get_post(id)
+
+    if request.method == 'POST':
+
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'A postagem deve ter um título'
+        elif not body:
+            error = 'A postagem deve ter conteúdo'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ? '
+                'WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+        
+    return render_template('blog/update.html', post=post)
