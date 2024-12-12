e881a4a9cfb44a6dfe9d28e10b076f07db9e6545
KauanHK <kauankaestner06@gmail.com>
2024-12-12 08:49:29 -0300

    feat: view de registro de usuário

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -7,3 +7,34 @@ from .db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Nome de usuário obrigatório.'
+        elif not password:
+            error = 'Crie uma senha.'
+
+        if error is None:
+            try:
+                db.execute(
+                    'INSERT INTO user (username, password) VALUES (?,?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f'Usuário {username} já registrado.'
+            else:
+                return redirect(url_for('auth.login'))
+    
+        flash(error)
+    
+    return render_template('auth/register.html')
