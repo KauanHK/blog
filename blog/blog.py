532fd4046d103a1ad302d46a585b5820c35c4d96
KauanHK <kauankaestner06@gmail.com>
2024-12-12 10:52:37 -0300

    Adicionado view delete() para deletar post

--- blog/blog.py
+++ blog/blog.py
@@ -98,3 +98,20 @@ def update(id: int):
             return redirect(url_for('blog.index'))
         
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id: int):
+
+    # Verifica se o post existe e se o autor é o usuário logado
+    # Se não, é lançada uma exceção
+    get_post(id)
+
+    db = get_db()
+    db.execute(
+        'DELETE FROM post WHERE id = ?',
+        (id,)
+    )
+    db.commit()
+    return redirect(url_for('blog.index'))
