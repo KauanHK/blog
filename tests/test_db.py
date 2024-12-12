474ad044034cd9245aad10295c4a76741b4cadf2
KauanHK <kauankaestner06@gmail.com>
2024-12-12 17:55:46 -0300

    Adicionado test_init_db_command() para testar o comando init-db

--- tests/test_db.py
+++ tests/test_db.py
@@ -14,3 +14,17 @@ def test_get_close_db(app: Flask):
         db.execute('SELECT 1')
 
     assert 'closed' in str(e.value)
+
+
+def test_init_db_command(runner, monkeypatch):
+    
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('blog.db.init_db', fake_init_db)
+    result = runner.invoke(args = ['init-db'])
+    assert 'inicializado' in result.output
+    assert Recorder.called
