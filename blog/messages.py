4f34836b9d05d9ff84245f5f9296b5dd17a263e2
KauanHK <kauankaestner06@gmail.com>
2024-12-13 10:40:11 -0300

    Adicionado messages.py

---
+++ blog/messages.py
@@ -0,0 +1,11 @@
+USERNAME_INVALIDO = 'Username inválido.'
+SENHA_INVALIDA = 'Defina uma senha.'
+USERNAME_JA_REGISTRADO = 'Username já registrado.'
+USERNAME_INCORRETO = 'Username incorreto.'
+SENHA_INCORRETA = 'Senha incorreta.'
+
+POST_NAO_EXISTE = 'Post não existe.'
+SEM_TITULO = 'A postagem deve ter um título.'
+SEM_BODY = 'A postagem deve ter conteúdo.'
+
+INIT_DB_MESSAGE = 'Banco de dados inicializado.'
