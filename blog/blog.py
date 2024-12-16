4ff5497fe3c09c3271abc0729d58ba3b229924eb
KauanHK <kauankaestner06@gmail.com>
2024-12-16 15:51:33 -0300

    adicionado view reply()

--- blog/blog.py
+++ blog/blog.py
@@ -175,3 +175,28 @@ def like(post_id: int):
         'like_count': like_count
     })
 
+
+
+@bp.route('/reply/<int:post_id>', methods=('GET', 'POST'))
+@login_required
+def reply(post_id: int = None):
+
+    get_post(post_id, check_author=False)
+    if request.method == 'GET':
+        return render_template('blog/reply.html')
+
+    db = get_db()
+    body = request.form['body']
+
+    if not body:
+        flash(SEM_BODY)
+
+    else:
+
+        db.execute(
+            'INSERT INTO reply (post_id, user_id, body) VALUES (?,?,?)',
+            (post_id, g.user['id'], body)
+        )
+        db.commit()
+
+    return redirect(url_for('blog.index'))
