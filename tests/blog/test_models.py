2eff898acac7e012e24080c7185dfc9e4877bfe5
KauanHK <kauankaestner06@gmail.com>
2024-12-18 10:49:49 -0300

    Adicionado testes para User.create()_and_save e User.get()

--- tests/blog/test_models.py
+++ tests/blog/test_models.py
@@ -1,10 +1,13 @@
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
+import sqlite3
 
 from conftest import AuthActions
 from blog.db import get_db
-from blog.models import User, Post
+from blog.models import User, Post, ModelType
+from typing import Any
+from collections.abc import Callable
 
 
 
@@ -14,6 +17,7 @@ def get_user(username: str):
         'SELECT username FROM user WHERE username = ?',
         (username,)
     ).fetchone()
+
 
 def test_save_user(app: Flask):
 
@@ -27,3 +31,36 @@ def test_save_user(app: Flask):
 
         bob = get_user(username)
         assert bob['username'] == username
+
+
+def test_user_create_and_save(app: Flask):
+
+    username = 'robert'
+    with app.app_context():
+
+        User.create_and_save(username, '123')
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?',
+            (username,)
+        ).fetchone()
+
+        assert user['username'] == username
+
+
+@pytest.mark.parametrize(
+    ('username', 'password'),
+    (
+        ('bob', '123'),
+        ('a', 'a'),
+        ('z', 'z')
+    )
+)
+def test_user_get(app: Flask, username: str, password: str):
+
+    with app.app_context():
+
+        User.create_and_save(username, password, raise_integrity = False)
+
+        user = User.get(username = username)
+        assert user.username == username
